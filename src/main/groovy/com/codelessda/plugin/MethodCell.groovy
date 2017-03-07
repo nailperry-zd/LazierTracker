ce92df1da3546778aa66831a82ad00dbdff78d46
张丹 <hzzhangdan1@corp.netease.com>
2017-03-07 11:40:38 +0800

    修改参数注释

--- src/main/groovy/com/codelessda/plugin/MethodCell.groovy
+++ src/main/groovy/com/codelessda/plugin/MethodCell.groovy
@@ -15,11 +15,11 @@ public class MethodCell {
     String agentName
     // 采集数据的方法描述
     String agentDesc
-    // 采集数据的方法参数说明（ 0：this，1+：普通参数 ）
+    // 采集数据的方法参数起始索引（ 0：this，1+：普通参数 ）
     int paramsStart
     // 采集数据的方法参数个数
     int paramsCount
-    // 参数类型对应的ASM指令
+    // 参数类型对应的ASM指令，加载不同类型的参数需要不同的指令
     List<Integer> opcodes
 
     MethodCell(String name, String desc, String parent, String agentName, String agentDesc, int paramsStart, int paramsCount, List<Integer> opcodes) {
