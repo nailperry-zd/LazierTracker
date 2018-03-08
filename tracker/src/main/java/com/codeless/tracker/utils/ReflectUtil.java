e3a9157cf1c4588d1414f8863f330e135343b58a
zhangdan <hzzhangdan1@corp.netease.com>
2018-03-08 20:44:20 +0800

    feat(tracker): 支持反射获取父类成员变量

--- tracker/src/main/java/com/codeless/tracker/utils/ReflectUtil.java
+++ tracker/src/main/java/com/codeless/tracker/utils/ReflectUtil.java
@@ -11,25 +11,37 @@ public class ReflectUtil {
      * 获取Object对象，指定成员变量属性值
      */
     public static Object getObjAttr(String fieldName, Object obj) {
-        // 获取对象obj的所有属性域
-        Field[] fields = obj.getClass().getDeclaredFields();
         Object value = null;
-        for (Field field : fields) {
-            // 对于每个属性，获取属性名
-            String varName = field.getName();
-            if (varName.equals(fieldName)) {
-                try {
-                    boolean access = field.isAccessible();
-                    if (!access) field.setAccessible(true);
-                    //从obj中获取field变量
-                    value = field.get(obj);
-                    if (!access) field.setAccessible(false);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
+        Class tmpClass = obj.getClass();
+        while (tmpClass != null && !tmpClass.getName().toLowerCase().equals("java.lang.object")) {
+            // 获取对象obj的所有属性域
+            Field[] fields = tmpClass.getDeclaredFields();
+            boolean isHit = false;
+            for (Field field : fields) {
+                // 对于每个属性，获取属性名
+                String varName = field.getName();
+                if (varName.equals(fieldName)) {
+                    try {
+                        boolean access = field.isAccessible();
+                        if (!access) field.setAccessible(true);
+                        //从obj中获取field变量
+                        value = field.get(obj);
+                        if (!access) field.setAccessible(false);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    isHit = true;
+                    break;
                 }
+            }
+
+            if (isHit) {
                 break;
+            } else {
+                tmpClass = tmpClass.getSuperclass();
             }
         }
+
         return value;
     }
 
